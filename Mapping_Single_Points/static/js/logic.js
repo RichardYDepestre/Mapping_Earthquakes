7f08d842ecd5dbd94f3f45dd16e7c031bd440866
Richard Depestre <y.richard.depestre@gmail.com>
2021-05-14 18:38:37 -0400

    Update logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -20,6 +20,16 @@ let mymap = L.map("mapid", {
 //     accessToken: API_KEY,
 //   }
 // );
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.circleMarker([34.0522, -118.2437], {
+  radius: 25,
+  color: "black",
+  fillColor: "#ffffa1"
+}).addTo(mymap);
+// L.circle([34.0522, -118.2437], {
+//   radius: 700
+// }).addTo(mymap);
+// L.marker([34.0522, -118.2437]).addTo(mymap);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
