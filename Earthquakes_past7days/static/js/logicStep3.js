8386092d2629bbcb5b940c5aeadb223b4bd0ee9d
Richard Depestre <y.richard.depestre@gmail.com>
2021-05-16 00:46:42 -0400

    refresh logicStepX.js

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -12,11 +12,11 @@ let streets = L.tileLayer(
   });
 
 let satelliteStreets = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/satellite-streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  "https://api.mapbox.com/styles/v1/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 11,
+    maxZoom: 50,
     accessToken: API_KEY,
   });
 
@@ -31,28 +31,53 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
     pointToLayer: function (feature, latlng) {
+      console.log(feature.properties.mag);
       return L.circleMarker(latlng);
     },
     style: function styleInfo(feature) {
       return {
-        fillColor: "#ffae42",
         color: "#000000",
         opacity: 1,
         fillOpacity: 1,
         stroke: true,
-        radius: getRadius(feature.properties.magnitude),
-        weight: 0.5
+        radius: getRadius(feature.properties.mag),
+        weight: 0.5,
+        fillColor: getColor(feature.properties.mag)
       }
+    },
+
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+
     }
   }).addTo(mymap);
-
 });
+
 
 function getRadius(magnitude) {
   if (magnitude === 0) {
     return 1;
   }
   return magnitude * 4;
+}
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
 }
 
 // Create the map object with center, zoom level and default layer.
@@ -62,11 +87,5 @@ let mymap = L.map('mapid', {
   layers: [streets]
 });
 
-
-// Create a style for the lines.
-// let myStyle = {
-//   color: "black",
-//   weight: 0.75
-// }
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(mymap);
